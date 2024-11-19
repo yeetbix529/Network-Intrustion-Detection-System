6364dfc387e5195d03a407af99594f1923bf5648
yeetbix529 <josh.sul529@gmail.com>
2024-11-19 06:39:17 +0000

    basic auth changes

--- start_services.sh
+++ start_services.sh
@@ -8,10 +8,6 @@ sudo snort -c /etc/snort/snort.lua -i wlan0 -D -f || { echo "Failed to start Sno
 echo "Starting filebeat..."
 sudo systemctl start filebeat || { echo "Failed to start filebeat"; exit 1; }
 
-echo "Starting logstash..."
-cd /usr/share/logstash/bin
-sudo bash logstash --path.settings /etc/logstash || { echo "Failed to start logstash"; exit 1; }
-
 echo "Starting elasticsearch..."
 cd /usr/share/elasticsearch/bin
 sudo bash -u elasticsearch bash elasticsearch -d || { echo "Failed to start elasticsearch"; exit 1; }
